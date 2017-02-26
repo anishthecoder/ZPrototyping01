71c59ec19104bfd9137f3f52333cdf9b6c8d35a7
anishabraham <anish@wardoberlab.com>
2017-02-25 18:59:54 -0600

    Determined how to show constant message updates.

--- JavaFXLearning/src/anish/javafx/learning01/Controller.java
+++ JavaFXLearning/src/anish/javafx/learning01/Controller.java
@@ -3,10 +3,10 @@ package anish.javafx.learning01;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
+import javafx.geometry.*;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.*;
 import javafx.stage.*;
 
 public class Controller {
@@ -31,6 +31,7 @@ public class Controller {
 			public Void call()
 					throws InterruptedException {
 				for (int i = 0; i < 10; i++) {
+					updateMessage("Evaluating " + i);
 					updateProgress(i, 10);
 					Thread.sleep(200);
 				}
@@ -56,30 +57,41 @@ public class Controller {
 	public static class ProgressDialog {
 		private final Stage dialogStage;
 		private final ProgressBar pb = new ProgressBar();
+		private final String title = "Evaluating...";
+		private final Label mssg = new Label();
 
 		public ProgressDialog() {
 			dialogStage = new Stage();
 			dialogStage.initStyle(StageStyle.UTILITY);
 			dialogStage.setResizable(false);
 			dialogStage.initModality(Modality.APPLICATION_MODAL);
+			dialogStage.setTitle(title);
+			dialogStage.setWidth(200);
+			dialogStage.setOnCloseRequest(evt -> {
+				evt.consume();
+			});
 
 			// PROGRESS BAR
-			final Label label = new Label();
-			label.setText("Calculating ");
-
 			pb.setProgress(-1F);
+			HBox.setMargin(pb, new Insets(5));
 
 			final HBox hb = new HBox();
-			hb.setSpacing(5);
+			hb.setSpacing(15);
 			hb.setAlignment(Pos.CENTER);
 			hb.getChildren().add(pb);
 
-			final Scene scene = new Scene(hb);
+			final BorderPane dgContainer = new BorderPane();
+			dgContainer.setCenter(hb);
+			dgContainer.setTop(mssg);
+			BorderPane.setMargin(mssg, new Insets(5));
+
+			final Scene scene = new Scene(dgContainer);
 			dialogStage.setScene(scene);
 		}
 
 		public void activateProgressBar(final Task<?> task) {
 			pb.progressProperty().bind(task.progressProperty());
+			mssg.textProperty().bind(task.messageProperty());
 			dialogStage.show();
 		}
 
