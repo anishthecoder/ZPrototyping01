a82aaa3a50c7bc23d43ac556c714fb869d33fa8a
Anish V. Abraham <anish@wardoberlab.com>
2017-11-10 11:38:37 -0600

    Resolving Java9 RCP compilation issues

---
+++ soq/q0005a/src/com/anish/soq/q0005a/handlers/FXShellTest.java
@@ -0,0 +1,82 @@
+
+package com.anish.soq.q0005a.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ShellEvent;
+import org.eclipse.swt.events.ShellListener;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+import javafx.embed.swt.FXCanvas;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+public class FXShellTest {
+
+	protected Shell openShell = null;
+
+	@Execute
+	public void execute() {
+
+		if (openShell != null) {
+			openShell.forceFocus();
+			return;
+		}
+
+		/* Prepare the shell for an FX Canvas */
+		final Shell dlgShell = new Shell(Display.getDefault(), SWT.DIALOG_TRIM);
+		dlgShell.setLayout(new FillLayout());
+		dlgShell.setText("Publish Images...");
+
+		Group group = new Group();
+		Scene scene = new Scene(group);
+		Button button = new Button("Dialog Button");
+		button.setOnAction(evt -> {
+			System.out.println(evt);
+		});
+		group.getChildren().add(button);
+
+		final FXCanvas canvas = new FXCanvas(dlgShell, SWT.NONE);
+		canvas.setScene(scene);
+
+		/* Dialog opening is not blocking. */
+		dlgShell.pack();
+		dlgShell.open();
+
+		openShell = dlgShell;
+
+		/*
+		 * Add a listener to ensure that multiple instances of the dialog are not open
+		 * and to disconnect the tool as a listener to other entities in the
+		 * application. when it is closed.
+		 */
+		dlgShell.addShellListener(new ShellListener() {
+
+			@Override
+			public void shellIconified(final ShellEvent e) {
+			}
+
+			@Override
+			public void shellDeiconified(final ShellEvent e) {
+			}
+
+			@Override
+			public void shellDeactivated(final ShellEvent e) {
+			}
+
+			@Override
+			public void shellClosed(final ShellEvent e) {
+
+				openShell = null;
+			}
+
+			@Override
+			public void shellActivated(final ShellEvent e) {
+			}
+		});
+
+	}
+}
---
+++ soq/q0005a/src/com/anish/soq/q0005a/parts/FXTestPart.java
@@ -0,0 +1,34 @@
+
+package com.anish.soq.q0005a.parts;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+
+import javafx.embed.swt.FXCanvas;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+public class FXTestPart {
+
+	private Scene createScene() {
+		Group group = new Group();
+		Scene scene = new Scene(group);
+		Button button = new Button("JFX Button");
+		group.getChildren().add(button);
+		return scene;
+	}
+
+	@PostConstruct
+	public void createComposite(final Composite parent) {
+		parent.setLayout(new FillLayout());
+
+		FXCanvas canvas = new FXCanvas(parent, SWT.NONE);
+
+		Scene scene = createScene();
+		canvas.setScene(scene);
+	}
+}
